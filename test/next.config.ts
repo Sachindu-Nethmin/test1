20a989b3a5780d6ad0e1899c133b6bb63d78de85
SachinduNethmin <108050026+Sachindu-Nethmin@users.noreply.github.com>
2024-12-20 11:18:52 +0530

    outt

--- test/next.config.ts
+++ test/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   basePath: "/test",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/test" : "",
 };
 
 export default nextConfig;
